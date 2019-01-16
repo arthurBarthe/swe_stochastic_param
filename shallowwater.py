1089e5b4ac88d783b125337a1f75e7e5f76d88d6
Tom Bolton <bolton@atmlxdt042.atm.ox.ac.uk>
2019-01-16 14:22:45 +0000

    add functionality to init from .nc file

--- shallowwater.py
+++ shallowwater.py
@@ -43,7 +43,8 @@ class ShallowWaterModel :
     ####################################################################################################################
 
     def __init__(self, output_path='./', Nx=256, Ny=256, Lx=3840e3, Ly=3840e3, Nt=360*24*60*60, 
-                 dump_freq=30*24*60*60, dump_output=False, tau0=0.1, nu_lap=540, model_name='', run_name='0001' ) :
+                 dump_freq=30*24*60*60, dump_output=False, tau0=0.1, nu_lap=540, model_name='', 
+                 run_name='0001' ) :
 
         """
         Initialise parameters for the model.
@@ -207,7 +208,7 @@ class ShallowWaterModel :
         print("\t ...set_timestep:: calculated timestep is dt = {} seconds.".format( self.dt ) )
         print("\t ...set_timestep:: total number of iterations to run for is N_iter = {}.".format( int( self.N_iter ) ) )
 
-    def set_initial_cond(self, init='rest', u_init=None, v_init=None, eta_init=None ) :
+    def set_initial_cond(self, init='rest', u_file=None, v_file=None, eta_file=None ) :
         """
         Initialise the prognostic variables of the model either
         
@@ -230,9 +231,22 @@ class ShallowWaterModel :
         elif init == 'state' :
 
             # load from .nc file
-            u_0 = u_init
-            v_0 = v_init
-            eta_0 = eta_init
+            u_data = Dataset( u_file )
+            v_data = Dataset( v_file ) 
+            eta_deta = Dataset( eta_file )
+            
+            u = np.array( u_data.variables['u'] )
+            v = np.array( v_data.variables['v'] )
+            eta = np.array( eta_data.variables['eta'] )
+            
+            if len( u.shape ) == 3 :
+				u_0 = np.squeeze( u[-1,:,:].reshape( (-1,1) )
+				v_0 = np.squeeze( v[-1,:,:].reshape( (-1,1) )
+				eta_0 = np.squeeze( eta[-1,:,:].reshape( (-1,1) )
+		    else : 
+				u_0 = np.squeeze( u.reshape( (-1,1) ) )
+				v_0 = np.squeeze( v.reshape( (-1,1) ) )
+				eta_0 = np.squeeze( eta.reshape( (-1,1) ) )
             
         # keep a copy of the most recent values 
         # of each field
